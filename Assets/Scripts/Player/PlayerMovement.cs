9e72b1c1db82d8f9c9e6101b7690d5033cd32840
MutantOctopus <bhustus@utc4me.org>
2015-10-21 13:36:58 -0400

    Health UI, 10:05

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour {
         Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
         RaycastHit floorHit;
 
-        if (Physics.Raycast(camRay, out floorHit, camRayLength)) {
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
             Vector3 playerToMouse = floorHit.point - transform.position;
             playerToMouse.y = 0f;
             Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
