cd13a30858d379961e21a2b29418ccbe98b0a4a2
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2015-11-16 13:20:40 -0500

    Powerups Frameworking
    Begun

---
+++ Assets/Scripts/Player/Bullets/AbstractBullets.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractBullets : MonoBehaviour {
+    [SerializeField]
+    float fireRate;
+    [SerializeField]
+    int damage;
+    public abstract void Fire ();
+}
---
+++ Assets/Scripts/Player/PowerupManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PowerupManager : MonoBehaviour {
+    BulletsDict Powers = new BulletsDict ();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+class BulletsDict : Dictionary<string, AbstractBullets> { }
